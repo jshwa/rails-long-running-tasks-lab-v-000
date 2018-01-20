f9f2f2b93667fab3e8e438ba484ec0d034486c7d
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-20 23:17:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,7 +49,7 @@ class SongsController < ApplicationController
   def upload
     CSV.foreach(params["file"].path, headers: true) do |song|
       song = Song.create(title: song[0])
-      song.artist_name = Artist.where((name: song[1]).first_or_create
+      song.artist_name = Artist.where(name: song[1]).first_or_create
     end
     redirect_to songs_path
   end
